51b1197d5cadfb04efb8fe555b141213dbb3638d
NANO <File changed>
2022-08-21 00:16:25 +0600

    3 submission

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.loops.finalTask;
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
         for (int i = 1; i <= cathetusLength; i++) {
-            for (int j = 0; j <= cathetusLength -i-1; j++) {
+            for (int j = 0; j <= cathetusLength-i-1; j++) {
                 System.out.print(" ");
             }
             for (int l = i; l >= 1; l--) {
@@ -12,9 +12,9 @@ public class Pyramid {
             for (int r = 2; r <= i; r++) {
                 System.out.print(r);
             }
-            for (int j = cathetusLength - 1; j >= i; j--) {
-                System.out.print(" ");
-            }
+            //for (int j = cathetusLength - 1; j >= i; j--) {
+              //  System.out.print(" ");
+            //}
             System.out.println();
         }
     }
